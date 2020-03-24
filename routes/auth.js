87b0caeb31522f0e50036d773d57bd9d2170cd97
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-03-24 09:57:19 +0100

    add ability to login a user

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const User = require('../models/User');
 const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
 
 const Router = express.Router();
 
@@ -17,9 +21,9 @@ Router.post('/register', (req, res) => {
   }
 
   let { password, email } = req.body;
-  console.log(password)
+
   password = bcrypt.hashSync(password, 10);
-  console.log(password);
+
   const user = {
     email: req.body.email,
     password
@@ -36,24 +40,79 @@ Router.post('/register', (req, res) => {
           .then(resp => {
             return res.json({
               msg: 'User Registered Successfully',
-              data: resp
+              data: resp,
+              status: 'error'
             });
           })
           .catch(err => {
-            console.log(err);
-            return res.status(500).json('Error creating user');
+            return res
+              .status(500)
+              .json({ status: 'error', message: 'Error registering user' });
           });
       }
     })
     .catch(err => {
-      console.log(err);
-      return res.json('error');
+      return res.json({ status: 'error', message: 'Error registering user' });
     });
 });
 
 // @route   POST api/v1/auth/login
 // @desc    Login user
 // @access  Public
-Router.post('/login', () => {});
+Router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  errors.status = 'error';
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const { SECRET_OR_KEY } = process.env;
+
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+      }
+
+      //Check password match
+      bcrypt.compare(password, user.password, (err, match) => {
+        if (err) {
+
+          errors.msg = 'Unable to login, please try again';
+          return res.status(400).json(errors);
+        }
+
+        if (match === false) {
+          errors.password = 'Incorrect Password';
+
+          return res.status(401).json(errors);
+        }
+        const payload = {
+          id: user._id,
+          email: user.email
+        };
+
+        //Sign token
+        jwt.sign(payload, SECRET_OR_KEY, (err, token) => {
+          if (err) {
+            errors.msg = 'Unable To Login. Please try again';
+            return res.status(400).json(errors);
+          } else {
+            return res.json({
+              status: 'success',
+              token: `Bearer ${token}`
+            });
+          }
+        });
+      });
+    })
+    .catch(err => {
+      errors.msg = 'Unable To Login. Please try again';
+
+      return res.status(400).json(errors);
+    });
+});
 
 module.exports = Router;
